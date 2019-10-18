7d26d1310f573c796cb683cbeefa5b5dd19a2f38
root <root@wezzel.com>
2019-10-18 14:14:34 -0600

    cleaned up basketball board.

--- b-board/v_set.php
+++ b-board/v_set.php
@@ -2,7 +2,7 @@
  header('Content-Type: text/event-stream');
  header('Cache-Control: no-cache');
  $v_set = fopen("../board/v-set.txt", "r") or die("Unable to open file!");
- $set = "0";
+ $set = "&#x21D0;";
  $set = fgets($v_set);
  echo "retry: 100\n";
  echo "data: ${set}\n\n";
